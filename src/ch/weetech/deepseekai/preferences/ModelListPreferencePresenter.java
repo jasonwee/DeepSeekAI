dba3826d933acb0ba9f84d014f13a82c6d4db43d
Jason Wee <peichieh@gmail.com>
2025-07-26 12:56:41 +0800

    added ModelListPreferencePresenter constructor

--- src/ch/weetech/deepseekai/preferences/ModelListPreferencePresenter.java
+++ src/ch/weetech/deepseekai/preferences/ModelListPreferencePresenter.java
@@ -1,5 +1,15 @@
 package ch.weetech.deepseekai.preferences;
 
+import org.eclipse.jface.preference.IPreferenceStore;
+
 public class ModelListPreferencePresenter {
+
+    private final IPreferenceStore preferenceStore;
+    private ModelListPreferencePage view;
+
+    public ModelListPreferencePresenter(IPreferenceStore preferenceStore)
+    {
+        this.preferenceStore = preferenceStore;
+    }
 
 }
