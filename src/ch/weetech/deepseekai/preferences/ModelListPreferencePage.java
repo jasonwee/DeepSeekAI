e916f5868c6c39778599d0d3138cf2fc923d825f
Jason Wee <peichieh@gmail.com>
2025-10-26 11:51:30 +0800

    added method createContents

--- src/ch/weetech/deepseekai/preferences/ModelListPreferencePage.java
+++ src/ch/weetech/deepseekai/preferences/ModelListPreferencePage.java
@@ -1,8 +1,19 @@
 package ch.weetech.deepseekai.preferences;
 
+import java.util.stream.Stream;
+
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.custom.SashForm;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Table;
+import org.eclipse.swt.widgets.TableColumn;
+
 public class ModelListPreferencePage extends PreferencePage implements IWorkbenchPreferencePage {
 
-	// inject
+    // inject
     private UISynchronize uiSync;
     
     private ModelListPreferencePresenter presenter;
@@ -35,5 +46,47 @@ public class ModelListPreferencePage extends PreferencePage implements IWorkbenc
         // be handled by the eclipse context
         IEclipseContext eclipseContext = workbench.getService(IEclipseContext.class);
         uiSync = eclipseContext.get(UISynchronize.class);
+    }
+
+    @Override
+    protected Control createContents(Composite parent) {
+        // change orientation to HORIZONTAL for side-by-side layout
+        var sashForm = new SashForm(parent, SWT.VERTICAL);
+        sashForm.setLayoutData(new GridData(GridData.FILL_BOTH));
+
+        // Composite for list and buttons
+        Composite listButtonsComposite = new Composite(sashForm, SWT.NONE);
+        listButtonsComposite.setLayout(new GridLayout(2, false));
+
+        modelTable = new Table(listButtonsComposite, SWT.BORDER | SWT.SINGLE | SWT.V_SCROLL);
+        modelTable.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true));
+        modelTable.setHeaderVisible(true);
+        Stream.of("Url", "Model Name").forEach(columnName -> {
+            TableColumn column = new TableColumn(modelTable, SWT.NULL);
+            column.setText(columnName);
+        });
+
+        // Composite for buttons to align them vertically
+        Composite buttonComposite = new Composite(listButtonsComposite, SWT.NONE);
+        buttonComposite.setLayout(new GridLayout(1, false));
+        buttonComposite.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false));
+
+        removeButton = new Button(buttonComposite, SWT.NONE);
+        removeButton.setText("Remove");
+        removeButton.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false));
+
+        // Model details in the bottom part of the sash form
+        createModelDetails(sashForm);
+
+        // Adjust the weights to allocate space (.e.g, 30% for list and buttons,
+        // 70% for details)
+        sashForm.setWeights(new int[] {1, 2});
+
+        presenter.registerView(this);
+
+        initializeListeners();
+        clearModelDetails();
+
+        return sashForm;
     }
 }
