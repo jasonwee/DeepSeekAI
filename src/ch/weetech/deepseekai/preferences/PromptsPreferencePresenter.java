737283f475c3d2106c7eb257abf60ed7c2a1d105
Jason Wee <peichieh@gmail.com>
2026-08-01 01:23:59 +0800

    initial

---
+++ src/ch/weetech/deepseekai/preferences/PromptsPreferencePresenter.java
@@ -0,0 +1,10 @@
+package ch.weetech.deepseekai.preferences;
+
+import org.eclipse.jface.preference.IPreferenceStore;
+
+public class PromptsPreferencePresenter {
+
+    private PromptsPreferencePage view;
+    private IPreferenceStore preferenceStore;
+
+}
